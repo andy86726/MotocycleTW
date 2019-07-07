06d06fcc39efc6c63f775b902b6866a23f2e0f96
曾子昂 <andy86726@gmail.com>
2019-07-07 17:27:08 +0800

    first commit

---
+++ MotocycleTW/Models/Delive_Way.cs
@@ -0,0 +1,18 @@
+﻿using MotocycleTW.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MotocycleTW.Models
+{
+    public class Delive_Way
+    {
+        [Key]
+        public int dw_id { get; set; }
+        public string dw_name { get; set; }
+
+        public virtual ICollection<Order> Order { get; set; }
+    }
+}
